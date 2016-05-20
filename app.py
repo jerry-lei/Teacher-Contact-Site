ad485990d71ed6646309b19b423d17578066f067
RJZheng1 <stuykid2016@yahoo.com>
2016-05-20 10:13:12 -0400

    Gmail doesn't work :(

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, session, request, redirect
 import database, hashlib
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/index.html", current_user = session.get('username'), auth = session.get('auth'))
+    return render_template("index.html", current_user = session.get('username'), auth = session.get('auth'))
 
 @app.route("/teacher", methods=["GET", "POST"])
 def teacher():
@@ -13,7 +15,7 @@ def teacher():
         if session.get('username') != None:
             return redirect("/")
         else:
-            return render_template("/login.html")
+            return render_template("login.html")
     else:
         username = request.form.get("login")
         session['username'] = username
@@ -26,7 +28,7 @@ def student():
         if session.get('user') != None:
             return redirect("/")
         else:
-            return render_template("/login.html")
+            return render_template("login.html")
     else:
         username = request.form.get("login")
         session['user'] = username
@@ -38,6 +40,13 @@ def logout():
     session.clear()
     return redirect("/")
     
+@app.route("/gmail")
+def gmail():
+    with open('../secret_key/gmail.json') as data_file:
+        data = json.load(data_file)
+    client_id = data['private_key_id'] 
+    return render_template("gmail.html", client_id = client_id)
+
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = "supersecretkey"
